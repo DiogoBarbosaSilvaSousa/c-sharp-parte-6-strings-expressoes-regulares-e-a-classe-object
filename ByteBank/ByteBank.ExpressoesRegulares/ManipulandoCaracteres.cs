b3e2728516551eb5815828a5b3034d7516696090
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-08-26 09:52:30 -0300

    Mais Sobre a String

---
+++ ByteBank/ByteBank.ExpressoesRegulares/ManipulandoCaracteres.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.ExpressoesRegulares
+{
+    /// <summary>
+    /// Biblioteca que mostra como manipular strings no C#
+    /// </summary>
+    public class ManipulandoCaracteres
+    {
+        /// <summary>
+        /// Mostra como o método Substring funciona.
+        /// </summary>
+        /// <param name="texto">Texto a ser modificado</param>
+        /// <param name="inicia">Número do caractere onde o texto se inicia.</param>
+        /// <returns></returns>
+        public static string TextoSubstring(string texto, int inicia)
+        {
+            string textoAlterado = texto;
+
+            return textoAlterado.Substring(inicia);
+        }
+
+    }
+}
---
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,26 @@
+﻿using ByteBank.ExpressoesRegulares;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadLine();
+        }
+
+        public static void TesteSubstring()
+        {
+
+            Console.WriteLine(ManipulandoCaracteres.TextoSubstring("pagina?argumentos", 7));
+        }
+
+    }
+}
